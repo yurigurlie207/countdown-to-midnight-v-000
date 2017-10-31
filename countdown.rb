6bc471b5a8cf40555db11f6c623ffb460b7c5ea0
Adrienne Yao <adrienne.yao@icloud.com>
2017-10-31 18:10:56 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(count)
+  while count > 0 do
+    puts "#{count} SECOND(S)!"
+    count -= 1
+  end
+
+"HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(count)
+  while count > 0 do
+    puts "#{count} SECOND(S)!"
+    sleep(1)
+    count -= 1
+  end
+
+"HAPPY NEW YEAR!"
 end
